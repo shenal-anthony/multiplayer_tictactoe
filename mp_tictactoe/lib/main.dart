0d4d6a6680abd6d06b7424bb896e61274c52329e
shenal-anthony <fonseka-im21039@stu.kln.ac.lk>
2025-07-24 18:37:46 +0530

    make it responsive

--- mp_tictactoe/lib/main.dart
+++ mp_tictactoe/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:mp_tictactoe/screens/create_room.dart';
+import 'package:mp_tictactoe/screens/join_room.dart';
 import 'package:mp_tictactoe/screens/main_menu.dart';
 import 'package:mp_tictactoe/utils/colors.dart';
 
@@ -14,9 +16,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: const Color.fromARGB(204, 7, 7, 40)),
-      home: const MainMenuScreen(),
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: const Color.fromARGB(204, 7, 7, 40),
+      ),
+      routes: {MainMenuScreen.routeName: (context) => const MainMenuScreen()
+      ,
+      JoinRoomScreen.routeName: (context) => const JoinRoomScreen(),
+      CreateRoomScreen.routeName: (context) => const CreateRoomScreen()},
+      initialRoute: MainMenuScreen.routeName,
     );
   }
 }
-
--- mp_tictactoe/lib/screens/main_menu.dart
+++ mp_tictactoe/lib/screens/main_menu.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:mp_tictactoe/responsive/responsive.dart';
+import 'package:mp_tictactoe/screens/create_room.dart';
+import 'package:mp_tictactoe/screens/join_room.dart';
+import 'package:mp_tictactoe/widgets/custom_button.dart';
 
 class MainMenuScreen extends StatelessWidget {
+  static const String routeName = '/main-menu';
   const MainMenuScreen({super.key});
+
+  void createRoom(BuildContext context) {
+    Navigator.pushNamed(context, CreateRoomScreen.routeName);
+  }
+
+  void joinRoom(BuildContext context) {
+    Navigator.pushNamed(context, JoinRoomScreen.routeName);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('Welcome to the Main Menu')));
+    return Scaffold(
+      body: Responsive(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CustomButton(onTap: () => createRoom(context), text: 'Create Room'),
+            const SizedBox(height: 20),
+            CustomButton(onTap: () => joinRoom(context), text: 'Join Room'),
+          ],
+        ),
+      ),
+    );
   }
 }
